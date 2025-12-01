4ec6edcb91ec2fb1432149fd51c2573fc42e9979
Luis Zermeno <lgzg90@hotmail.com>
2025-12-01 22:23:31 +0100

    feature: add memory to agent for conversation

--- agents/agents.py
+++ agents/agents.py
@@ -1,9 +1,10 @@
 
+from langgraph.graph.state import Checkpoint
 import yaml
 import os
 from langchain.agents import create_agent
 from dotenv import load_dotenv
-
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
 
@@ -20,5 +21,6 @@ test_prompt = prompts["test_prompt"]
 
 financial_agent = create_agent(
     system_prompt=test_prompt,
-    model="anthropic:claude-haiku-4-5"
+    model="anthropic:claude-haiku-4-5",
+    checkpointer=InMemorySaver()
 )
--- main.py
+++ main.py
@@ -7,9 +7,13 @@ data = read_csvs_in_folder(year)
 #print(data)
 
 
-message = input(f"Your message:/n")
+while True:
+    message = input(f"Your message:\n")
 
-response = financial_agent.invoke({"role" : "user", "messages" : f"{message}"})
+    response = financial_agent.invoke(
+        {"role" : "user", "messages" : f"{message}"},
+        {"configurable" : {"thread_id" : "memo_001"}}
+        )
 
-for i, msg in enumerate(response["messages"]):
-    msg.pretty_print()
+    for i, msg in enumerate(response["messages"]):
+        msg.pretty_print()
