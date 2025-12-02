4c6b1376caa316b629f5c5ad01f675240f4a1f26
Luis Zermeno <lgzg90@hotmail.com>
2025-12-02 11:07:16 +0100

    documentation: README up to date

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+"""
+Personal Finances Agent
+Main entry point for the application
+"""
+
+
 from UI import demo
 
-#To create a public link, set `share=True` in `launch()`
-demo.launch()
+if __name__ == "__main__":
+    #To create a public link, set `share=True` in `launch()`
+    demo.launch()
