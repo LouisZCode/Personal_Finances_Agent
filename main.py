3fe502db7931305d227a1c082026ca2d0eb41d15
Luis Zermeno <lgzg90@hotmail.com>
2025-12-02 10:40:17 +0100

    feature: add nice Gradio UI for better interaction

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 
 from functions import read_csvs_in_folder
 from agents import financial_agent
-
-year = 2025
-data = read_csvs_in_folder(year)
-#print(data)
+import gradio as gr
 
 
-while True:
-    message = input(f"Your message:\n")
+
+def agent_response(message, history):
+    messages = history + [{"role": "user", "content": message}]
 
     response = financial_agent.invoke(
-        {"role" : "user", "messages" : f"{message}"},
+        {"messages" : messages},
         {"configurable" : {"thread_id" : "memo_001"}}
         )
 
-    for i, msg in enumerate(response["messages"]):
-        msg.pretty_print()
+    return response["messages"][-1].content
+
+
+demo = gr.ChatInterface(
+    fn=agent_response,
+    title="Personal Balance Sheet Expert"
+)
+
+demo.launch()
